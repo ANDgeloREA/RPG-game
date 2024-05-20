804ecbf5aacf8e9c5d5669eb35a682242b7ca851
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2024-05-20 12:38:25 +0200

    adapted to the code base for graphical improvements and added new features for NPCs, monsters and scenes during the game

--- dragon-repeller/index.js
+++ dragon-repeller/index.js
@@ -16,6 +16,11 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const pgImg = document.querySelector("#pg");
+const npcImg = document.querySelector("#npc");
+const monsterImg = document.querySelector("#monster");
+const backgroundImg = document.querySelector("#background");
+
 const weapons = [
   { name: "stick", power: 5 },
   { name: "dagger", power: 30 },
@@ -24,19 +29,22 @@ const weapons = [
 ];
 const monsters = [
   {
-    name: "slime",
+    name: "Slime",
     level: 2,
     health: 15,
+    img: "./assets/npc/slime.png",
   },
   {
-    name: "fanged beast",
+    name: "Goblin",
     level: 8,
     health: 60,
+    img: "./assets/npc/goblin.png",
   },
   {
-    name: "dragon",
+    name: "Dragon",
     level: 20,
     health: 300,
+    img: "./assets/npc/dragon.png",
   },
 ];
 const locations = [
@@ -44,29 +52,37 @@ const locations = [
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
     "button functions": [goStore, goCave, fightDragon],
-    text: 'You are in the town square. You see a sign that says "Store".',
+    text: 'Villager: "Hero, thank you for your deeds. You are now in the city square. Before resuming your quest go to the store to improve your equipment."',
+    background: "./assets/scene/town_square.jpg",
+    npc: "./assets/npc/villager.png"
   },
   {
     name: "store",
     "button text": [
-      "Buy 10 health (10 gold)",
+      "Buy a potion (10 gold)",
       "Buy weapon (30 gold)",
       "Go to town square",
     ],
-    "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store.",
+    "button functions": [buyPotion, buyWeapon, goTown],
+    text: 'Blacksmith: "Welcome to my store. Do you want to buy a potion to recover 10 health or a new weapon?"',
+    background: "./assets/scene/store.jpg",
+    npc: "./assets/npc/blacksmith.png"
   },
   {
     name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters.",
+    "button text": ["Fight slime", "Fight goblin", "Go to town square"],
+    "button functions": [fightSlime, fightGoblin, goTown],
+    text: 'Spirit: "You enter the cave. You see some monsters. Choose who you want to face and be careful!"',
+    background: "./assets/scene/cave.jpg",
+    npc: "./assets/npc/spirit.png"
   },
   {
     name: "fight",
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster.",
+    background: "./assets/scene/fight.jpg",
+    npc: "./assets/npc/spirit.png"
   },
   {
     name: "kill monster",
@@ -76,25 +92,33 @@ const locations = [
       "Go to town square",
     ],
     "button functions": [goTown, goTown, easterEgg],
-    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+    text: 'Spirit: "The monster screams "Arg!" as it dies. You gain experience points and find gold."',
+    background: "./assets/scene/kill_monster.jpg",
+    npc: "./assets/npc/spirit.png"
   },
   {
     name: "lose",
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
-    text: "You die. &#x2620;",
+    text: 'Spirit: "You die &#x2620;. The raging dragon destroyed the village but I can bring you back. Do you still want to save the village?"',
+    background: "./assets/scene/lose.jpg",
+    npc: "./assets/npc/spirit.png"
   },
   {
     name: "win",
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
-    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
+    text: 'Spirit: "You defeat the dragon and saved the village! YOU WIN THE GAME! &#x1F389;"',
+    background: "./assets/scene/win.jpg",
+    npc: "./assets/npc/spirit.png"
   },
   {
     name: "easter egg",
     "button text": ["2", "8", "Go to town square?"],
     "button functions": [pickTwo, pickEight, goTown],
-    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+    text: 'Spirit: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"',
+    background: "./assets/scene/easter_egg.jpg",
+    npc: "./assets/npc/spirit.png"
   },
 ];
 
@@ -112,28 +136,36 @@ function update(location) {
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
   text.innerHTML = location.text;
+  backgroundImg.src = location.background;
+  npcImg.src = location.npc;
 }
 
 function goTown() {
   update(locations[0]);
+  monsterImg.style.display = "none";
+  npcImg.style.display = "block";
 }
 
 function goStore() {
   update(locations[1]);
+  monsterImg.style.display = "none";
+  npcImg.style.display = "block";
 }
 
 function goCave() {
   update(locations[2]);
+  monsterImg.style.display = "none";
+  npcImg.style.display = "block";
 }
 
-function buyHealth() {
+function buyPotion() {
   if (gold >= 10) {
     gold -= 10;
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
   } else {
-    text.innerText = "You do not have enough gold to buy health.";
+    text.innerText = "You do not have enough gold to buy more potions.";
   }
 }
 
@@ -146,12 +178,12 @@ function buyWeapon() {
       let newWeapon = weapons[currentWeapon].name;
       text.innerText = "You now have a " + newWeapon + ".";
       inventory.push(newWeapon);
-      text.innerText += " In your inventory you have: " + inventory;
+      text.innerText += " In your inventory you have: " + inventory.join(", ");
     } else {
-      text.innerText = "You do not have enough gold to buy a weapon.";
+      text.innerText = 'Blacksmith: "You do not have enough gold to buy a weapon."';
     }
   } else {
-    text.innerText = "You already have the most powerful weapon!";
+    text.innerText = 'Blacksmith: "You already have the most powerful weapon!"';
     button2.innerText = "Sell weapon for 15 gold";
     button2.onclick = sellWeapon;
   }
@@ -163,9 +195,9 @@ function sellWeapon() {
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
     text.innerText = "You sold a " + currentWeapon + ".";
-    text.innerText += " In your inventory you have: " + inventory;
+    text.innerText += " In your inventory you have: " + inventory.join(", ");
   } else {
-    text.innerText = "Don't sell your only weapon!";
+    text.innerText = 'Blacksmith: "Don\'t sell your only weapon!"';
   }
 }
 
@@ -174,7 +206,7 @@ function fightSlime() {
   goFight();
 }
 
-function fightBeast() {
+function fightGoblin() {
   fighting = 1;
   goFight();
 }
@@ -187,9 +219,13 @@ function fightDragon() {
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
-  monsterStats.style.display = "block";
+  monsterStats.style.display = "flex";
+  monsterImg.style.display = "block";
+  npcImg.style.display = "none";
   monsterName.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
+  monsterName.innerText = monsters[fighting].name;
+  monsterImg.src = monsters[fighting].img;
 }
 
 function attack() {
@@ -240,14 +276,21 @@ function defeatMonster() {
   goldText.innerText = gold;
   xpText.innerText = xp;
   update(locations[4]);
+  monsterImg.style.display = "none";
+  npcImg.style.display = "block";
 }
 
 function lose() {
   update(locations[5]);
+  pgImg.style.display = "none";
+  monsterImg.style.display = "none";
+  npcImg.style.display = "block";
 }
 
 function winGame() {
   update(locations[6]);
+  monsterImg.style.display = "none";
+  npcImg.style.display = "block";
 }
 
 function restart() {
@@ -260,10 +303,14 @@ function restart() {
   healthText.innerText = health;
   xpText.innerText = xp;
   goTown();
+  monsterImg.style.display = "none";
+  pgImg.style.display = "block";
 }
 
 function easterEgg() {
   update(locations[7]);
+  monsterImg.style.display = "none";
+  npcImg.style.display = "block";
 }
 
 function pickTwo() {
